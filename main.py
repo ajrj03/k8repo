8f0ddd44c05262353d201b7095a182e39b8b8978
root <root@ip-172-31-34-90.ap-south-1.compute.internal>
2025-05-19 23:08:57 +0000

    third commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse  # optional but good practice
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="templates")  # No leading slash
 
-@app.get("/")
-def form_post(request: Request):
-    return templates.TemplateResponse('form.html', context={'request': request})
+@app.get("/", response_class=HTMLResponse)
+async def form_post(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
 
